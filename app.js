8d170def50c9fc6cae666321fc232ca745366e67
JeongB-L <jblee1152@gmail.com>
2022-08-31 00:13:23 +0900

    open

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+// use strict will make the javascript to only take what it strictly has
+const switcher = document.querySelector('.btn')
+switcher.addEventListener('click', function() { //this handles the event when click happened.
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme'); // get each theme here
+
+    const className = document.body.className;
+    if (className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
